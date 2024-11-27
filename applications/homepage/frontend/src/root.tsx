de32cafa675b46c535a3013300a82069099c5ce9
amabico <naoya.uemura@proton.me>
2024-11-27 16:45:38 +0900

    sample page

---
+++ applications/homepage/frontend/src/root.tsx
@@ -0,0 +1,30 @@
+import { component$ } from "@builder.io/qwik";
+import { isDev } from "@builder.io/qwik/build";
+import {
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
+} from "@builder.io/qwik-city";
+import { RouterHead } from "./components/router-head/router-head";
+import "./global.css"
+
+export default component$(() => {
+  return (
+    <QwikCityProvider>
+      <head>
+        <meta charset="utf-8" />
+        {!isDev && (
+          <link
+            rel="manifest"
+            href={`${import.meta.env.BASE_URL}manifest.json`}
+          />
+        )}
+        <RouterHead />
+        {!isDev && <ServiceWorkerRegister />}
+      </head>
+      <body lang="en">
+        <RouterOutlet />
+      </body>
+    </QwikCityProvider>
+  );
+});
---
+++ applications/homepage/frontend/src/routes/index.tsx
@@ -0,0 +1,27 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <section>
+        <div class="flex justify-center">
+          <div class="flex flex-col items-center">
+            <img src="images/amabie.png" height={50} width={200} />
+            <div class="text-center text-gray-300 text-xs">『肥後国海中の怪(アマビエの図)』（京都大学附属図書館所蔵）</div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "amabi.co",
+  meta: [
+    {
+      name: "description",
+      content: "amabi.co site description",
+    },
+  ],
+};
