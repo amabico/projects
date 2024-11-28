3bac1222bd28efaa1ecb1f17079758854d61aaab
amabico <naoya.uemura@proton.me>
2024-11-28 18:57:18 +0900

    update: code block not using wasm

--- applications/homepage/frontend/src/components/markdown/code.tsx
+++ applications/homepage/frontend/src/components/markdown/code.tsx
@@ -1,6 +1,9 @@
 import { component$, useSignal, useTask$ } from "@builder.io/qwik"
 import type { Code as CodeNode } from "mdast";
-import { codeToHtml } from "shiki";
+import { createHighlighterCore } from "shiki/core"
+import { createJavaScriptRegexEngine } from "shiki/engine/javascript"
+import nord from "shiki/themes/nord.mjs"
+import rust from "shiki/langs/rust.mjs"
 
 interface CodeProps {
   node: CodeNode
@@ -10,7 +13,13 @@ export const Code = component$<CodeProps>(({ node }) => {
   const html = useSignal<string>()
 
   useTask$(async () => {
-    html.value =  await codeToHtml(node.value, { lang: node.lang || "shell", theme: "nord" })
+    const highlighter = await createHighlighterCore({
+      themes: [nord],
+      langs: [rust],
+      engine: createJavaScriptRegexEngine()
+    })
+
+    html.value =  await highlighter.codeToHtml(node.value, { lang: node.lang || "shell", theme: "nord" })
   })
   return (
     <span dangerouslySetInnerHTML={html.value} />
